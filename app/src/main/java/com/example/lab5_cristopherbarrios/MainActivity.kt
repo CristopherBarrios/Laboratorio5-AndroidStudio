d03f90e8f41d7f48e69272f1789856ae62313606
Cristopher Barrios <cjrbarrios@gmail.com>
2019-04-26 23:13:38 -0600

    2

--- app/src/main/java/com/example/lab5_cristopherbarrios/MainActivity.kt
+++ app/src/main/java/com/example/lab5_cristopherbarrios/MainActivity.kt
@@ -11,14 +11,20 @@ class MainActivity : AppCompatActivity() {
 
     private var button3: Button? = null
     private var textView: TextView? = null
+    private var textView1: TextView? = null
+    private var textView2: TextView? = null
     private var contador: Int= 0
+    private var contador1: Int= 0
+    private var contador2: Int= 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button3 = findViewById(R.id.button3) as Button
+
         textView = findViewById(R.id.textView2) as TextView
+        textView1 = findViewById(R.id.textView4) as TextView
+        textView2 = findViewById(R.id.textView6) as TextView
 
 
 
@@ -40,5 +46,43 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"No se puede disminuir", Toast.LENGTH_LONG).show()
         }
     }
+
+    fun onClick3(v: View?) {
+        if(contador1>=0){
+            contador1++
+            textView1?.setText(Integer.toString(contador))
+        }else{
+            Toast.makeText(this,"No se puede disminuir", Toast.LENGTH_LONG).show()
+
+        }
+    }
+    fun onClick4(v: View?) {
+        if(contador1>0){
+            contador1--
+            textView1?.setText(Integer.toString(contador))
+        }else{
+            Toast.makeText(this,"No se puede disminuir", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun onClick5(v: View?) {
+        if(contador2>=0){
+            contador2++
+            textView2?.setText(Integer.toString(contador))
+        }else{
+            Toast.makeText(this,"No se puede disminuir", Toast.LENGTH_LONG).show()
+
+        }
+    }
+    fun onClick6(v: View?) {
+        if(contador2>0){
+            contador2--
+            textView2?.setText(Integer.toString(contador))
+        }else{
+            Toast.makeText(this,"No se puede disminuir", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
 
 }
